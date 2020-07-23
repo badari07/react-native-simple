b187c67ccc1067dc67057921efa270ed55c5eb92
badari07 <badari0793@gmail.com>
2020-07-23 10:22:56 +0530

    feat:setup native

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>hi</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
